78b749f965a92f6d92e20db387e858ace3278565
wontothree <prepared7913@naver.com>
2025-06-13 13:01:29 +0900

    add github pages

--- 0726/Raspberry Pi/neotest.py
+++
@@ -1,5 +0,0 @@
-import board
-import neopixel
-pixels = neopixel.NeoPixel(board.D21, 16)
-pixels[0] = (255,0,0)
-
--- 0727/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-while True:
-#     print(sender, receiver, command, data)
-#     if command == 'F':
-#         print('Command is Finsh!!')
-#         break
